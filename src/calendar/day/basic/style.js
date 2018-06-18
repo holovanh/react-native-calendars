8246cb0fabf4b9531a3c328afa0832e58f685488
helen <30346073+holovanh@users.noreply.github.com>
2018-06-18 19:26:41 +0300

    Update style.js

--- src/calendar/day/basic/style.js
+++ src/calendar/day/basic/style.js
@@ -7,8 +7,8 @@ export default function styleConstructor(theme={}) {
   const appStyle = {...defaultStyle, ...theme};
   return StyleSheet.create({
     base: {
-      width: 32,
-      height: 32,
+      width: 36,
+      height: 36,
       alignItems: 'center'
     },
     text: {
@@ -24,7 +24,7 @@ export default function styleConstructor(theme={}) {
     },
     selected: {
       backgroundColor: appStyle.selectedDayBackgroundColor,
-      borderRadius: 16
+      borderRadius: 18
     },
     todayText: {
       color: appStyle.todayTextColor
